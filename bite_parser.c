f6436dc313e030f362a00b7b48a22f0cf1a00638
IsaacTCB <116975602+IsaacTCB@users.noreply.github.com>
2026-01-21 03:23:52 -0300

    bite_parser.c: Refactored stuff

--- bite_parser.c
+++ bite_parser.c
@@ -24,7 +24,7 @@ typedef struct {
 } bite_header_t;
 
 typedef struct {
-    uint32_t data_offset;
+    uint32_t data_offset; // Is 0 for empty files
     uint32_t data_size;
     uint32_t reserved;
     char*    name;
@@ -32,6 +32,7 @@ typedef struct {
 } bite_entry_t;
 
 static uint8_t bite__fread(void* out_ptr, size_t size, FILE* file) {
+    // @todo: Wrap all bite__fread calls into a #define.
     return fread(out_ptr, size, 1, file) == size;
 }
 
@@ -71,10 +72,34 @@ static bite_status_e bite__entry_read(bite_entry_t* entry, FILE* file) {
     return BITE_OK;
 }
 
+static bite_status_e bite__entry_table_read(bite_entry_t** entry_table, size_t entry_count, FILE* file) {
+    bite_status_e status;
+
+    bite_entry_t* entries = (bite_entry_t*)malloc(sizeof(bite_entry_t) * entry_count);
+    for (size_t i = 0; i < entry_count; i++) {
+        bite_entry_t* entry = entries + i;
+        status = bite__entry_read(entry, file);
+        if (status != BITE_OK) {
+            // Free up all string data
+            for (size_t j = 0; j < i; j++) {
+                entry = entries + j;
+                free(entry->name); // This is annoying. Perhaps a memory arena would be more fitting?
+            }
+
+            free(entries);
+            return status;
+        }
+    }
+
+    *entry_table = entries;
+    return BITE_OK;
+}
+
 int main(int argc, char* argv[]) {
     const char* bite_filepath = "data.bite";
     FILE* file = fopen(bite_filepath, "rb");
     
+    // Parse header
     bite_header_t bite_header;
     bite_status_e status = bite__header_read(&bite_header, file);
     if (status != BITE_OK) {
@@ -82,24 +107,26 @@ int main(int argc, char* argv[]) {
         exit(3);
     }
 
+    // Load file entry table
     fseek(file, bite_header.file_entry_offset, SEEK_SET);
-    bite_entry_t* entries = (bite_entry_t*)malloc(sizeof(bite_entry_t)*bite_header.file_entry_count);
+    bite_entry_t* entry_table = NULL;
+    status = bite__entry_table_read(&entry_table, bite_header.file_entry_count, file);
+    if (status != BITE_OK) {
+        printf("Unable to parse file entry table. Err: %d", status);
+        exit(3);
+    }
+
+    // Print filenames
     for (size_t i = 0; i < bite_header.file_entry_count; i++) {
-        bite_entry_t* entry = entries + i;
-        status = bite__entry_read(entry, file);
-        if (status != BITE_OK) {
-            printf("Unable to parse file table. Err: %d", status);
-            exit(4);
-        }
+        bite_entry_t* entry = entry_table + i;
         printf("FILE: %s\n", entry->name);
     }
 
     // Free up all data
     for (size_t i = 0; i < bite_header.file_entry_count; i++) {
-        bite_entry_t* entry = entries + i;
+        bite_entry_t* entry = entry_table + i;
         free(entry->name); // This is annoying. Perhaps a memory arena would be more fitting?
     }
-    free(entries);
-
+    free(entry_table);
     return 0;
 }
