41660fcb503694ebff0faefed8ea95cc0a2bd346
IsaacTCB <116975602+IsaacTCB@users.noreply.github.com>
2026-01-24 18:54:53 -0300

    cmake: fix shared lib build
    Library now supports being compiled as a shared object!

--- include/bitefile/bite.h
+++ include/bitefile/bite.h
@@ -4,6 +4,28 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// If you are linking this lib as a dynamic lib,
+// then make BITEFILE_USE_SHARED is defined!
+
+#if defined(_WIN32)
+    // Building as dynamic lib on Windows
+    #if defined(BITEFILE_BUILD_LIB)
+        #define BITE_API __declspec(dllexport)
+    // Linking as dynamic lib on Windows
+    #elif defined(BITEFILE_USE_SHARED)
+        #define BITE_API __declspec(dllimport)
+    #endif
+#elif defined(__GNUC__)
+    // Building as shared object on Unix systems
+    #if defined(BITEFILE_BUILD_LIB)
+        #define BITE_API __attribute__((visibility("default")))
+    #endif
+#endif
+
+#ifndef BITE_API
+#define BITE_API
+#endif
+
 typedef enum {
     BITE_OK = 0,
     BITE_ERR_INVALID,      // Not a valid bite file!
@@ -15,16 +37,16 @@ typedef struct bite_packed bite_packed_t; // Bite packed archive handle
 typedef struct bite_file bite_file_t;     // Virtual file handle
 
 // Bite-packed open/close
-bite_packed_t* bite_packed_open(const char* filepath);   // Opens a bite-packed file.
-void           bite_packed_close(bite_packed_t* packed); // Closes a bite-packed file
+BITE_API bite_packed_t* bite_packed_open(const char* filepath);   // Opens a bite-packed file.
+BITE_API void           bite_packed_close(bite_packed_t* packed); // Closes a bite-packed file
 
 // File operations
-bite_file_t* bite_fopen(bite_packed_t* packed, const char* filepath);
-const char*  bite_fname(bite_file_t* file);
-size_t       bite_fsize(bite_file_t* file);
-size_t       bite_fread(void* dst, size_t size, bite_file_t* file);
-size_t       bite_ftell(bite_file_t* file);
-int          bite_fseek(bite_file_t* file, long int pos, int whence);
-void         bite_fclose(bite_file_t* file);
+BITE_API bite_file_t* bite_fopen(bite_packed_t* packed, const char* filepath);
+BITE_API const char*  bite_fname(bite_file_t* file);
+BITE_API size_t       bite_fsize(bite_file_t* file);
+BITE_API size_t       bite_fread(void* dst, size_t size, bite_file_t* file);
+BITE_API size_t       bite_ftell(bite_file_t* file);
+BITE_API int          bite_fseek(bite_file_t* file, long int pos, int whence);
+BITE_API void         bite_fclose(bite_file_t* file);
 
 #endif // BITE_IMPL_H
